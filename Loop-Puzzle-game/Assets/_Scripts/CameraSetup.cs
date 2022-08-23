a492de782465d7f391132b9cfcc9f8a66cd26660
soheil <baayani.2.82@gmail.com>
2022-08-23 17:54:51 +0430

    whole level shot

--- Loop-Puzzle-game/Assets/_Scripts/CameraSetup.cs
+++ Loop-Puzzle-game/Assets/_Scripts/CameraSetup.cs
@@ -6,9 +6,17 @@ using Cinemachine;
 public class CameraSetup : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera cam;
+    [SerializeField] private Transform wholeLevelTarget;
+    [SerializeField] private float size;
 
     void Awake()
     {
         cam.Follow = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+    public void wideLevelShot()
+    {
+        cam.Follow = wholeLevelTarget;
+        cam.m_Lens.OrthographicSize = size;
     }
 }
